4eb06d2b1a2db3ebf51ab66cd1cf4ade30d089fe
PreciousDipsy <68125559+PreciousDipsy@users.noreply.github.com>
2020-11-04 21:19:25 +0200

    another commit

--- js/script.js
+++ js/script.js
@@ -1,44 +1,137 @@
 const listsContainer = document.query.Selector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitleElement = document.querySelector('[data-list-title]')
+const listCountElement = document.querySelector('[data-list-count]')
+const tasksContainer = document.querySelector('[data-tasks]')
+const taskTemplate = document.getElementById('task-template')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const newTaskInput = document.querySelector('[data-new-task-input]')
+cons clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]')
 
-let lists = [{
-    id: 1,
-    name: 'name'
-}, {
-    id: 2,
-    name: 'todo'
-}]
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListID'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e =>{
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+
+tasksContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'input') {
+        const selectedList = lists.find(list => list.id === selectedListId)
+        const selectedTask = selectedList.tasks.find(task => === e.target.id)
+        selectedTask.complete = e.target.checked
+        save()
+        renderTaskCount(selectedList)
+    }
+})
+
+clearCompleteTasksButton.addEventListener('click', e => {
+    const selectedListID = lists.find(list => list.id === selectedListId)
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete)
+    saveAndRender()
+})
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
 
 newListForm.addEventListener('submit', e => {
-e.preventDefault()
-const listName = newListInput.value
-if (listName == null || listName === '') return
-const list = createList(listName)
-newListInput.value = null
-lists.push(list)
-render()
-})
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    saveAndRender()
+    })
+
+    newTaskForm.addEventListener('submit', e => {
+        e.preventDefault()
+        const taskName = newTaskInput.value
+        if (listName == null || listName === '') return
+        const list = createTask(listName)
+        newTaskInput.value = null
+        const selectedList = lists.find(list => list.id === selectedListId)
+        selectedList.tasks.push(task)
+        saveAndRender()
+        })
 
 function createList(name) {
     return { id: Date.now().toString(), name: name, tasks: [] }
 }
 
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false [] }
+}
+
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
 function render() {
-    <li class="list-name">Lightroom</li>
     clearElement(listsContainer)
+    renderLists()
+    const selectedList = lists.find(list  => listId === selectedListId)
+
+    if (selectedListId == null) {
+        listDisplayContainer.style.display = 'none'
+    } else {
+        listDisplayContainer.style.display= ''
+        listTitleElement.innerText = selectedListId.name
+        renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
+    }
+}
+
+
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+       const taskElement = document.importNode(taskElement.content, true) 
+       const checkbox = taskElement.querySelector('input')
+       checkbox.Id = task.id
+       checkbox.checked = task.complete
+       const label = taskElement.querySelector('label')
+       label.htmlFor = task.id 
+       label.append(task.name)
+       tasksContainer.appendChild(taskElement)
+    })
+}
+
+function renderTaskCount(selectedList) {
+  const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length
+  const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+  listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+}
+
+function renderLists () {
     lists.forEach(list => {
         const listElement = document.createElement('li')
         listElement.dataset.listId = listId
         listElement.classList.add("list-name")
+        if (list.id === selectedListId) {
+         listElement.classList.add(active-list)
         listElement.innerText = list.name
+        }    
         listsContainer.appendChild(listElement)
     })
 }
 
 function clearElement(element){
     while (element.firstChild) {
-        element.removeChild(elemnt.firstChild)
+        element.removeChild(element.firstChild)
     }
 
 }
